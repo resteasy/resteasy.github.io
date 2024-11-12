8c07e3977dbf30e0ffdeb51dccff87afbd5549c5
James R. Perkins <jperkins@redhat.com>
2024-11-11 17:04:17 -0800

    Add documentation for 7.0.0.Alpha4 and 6.2.11.Final.
    Signed-off-by: James R. Perkins <jperkins@redhat.com>

---
+++ static/6.2/javadocs/tag-search-index.js
@@ -0,0 +1,1 @@
+tagSearchIndex = [{"l":"Constant Field Values","h":"","u":"constant-values.html"},{"l":"Constructor","h":"class org.jboss.resteasy.spi.PriorityServiceLoader","d":"Section","u":"org/jboss/resteasy/spi/PriorityServiceLoader.html#constructorArg"},{"l":"Serialized Form","h":"","u":"serialized-form.html"}];updateSearchResults();
